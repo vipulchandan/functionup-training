0548d1d7b8b8faea4a88e68687b01b8eaf6d5857
vipulchandan <plenteousvipul90009@gmail.com>
2023-05-20 08:50:08 +0530

    Added status code and exception handling

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,15 +1,37 @@
 const BookModel = require('../models/bookModel');
 
 const createBook = async (req, res) => {
-    let data = req.body;
-    let createdBook = await BookModel.create(data);
+    try {
+        let data = req.body;
+        let createdBook = await BookModel.create(data);
 
-    res.send({ msg: createdBook });
+        res.status(201).send({
+            status : true,
+            message : 'Book created successfully',
+            data : createdBook
+        })
+    } catch (err) {
+        res.status(500).send({
+            status : false,
+            message : err.message
+        })
+    }
 }
 
 const getAllBooks = async (req, res) => {
-    let getBooks = await BookModel.find()
-    res.send({ msg : getBooks });
+    try {
+        let getBooks = await BookModel.find();
+        res.status(200).send({
+            status : true,
+            message : 'Books fetched successfully',
+            data : getBooks
+        })
+    } catch (err) {
+        res.status(500).send({
+            status : false,
+            message : err.message
+        })
+    }
 }
 
 module.exports = { createBook, getAllBooks }
