005618f8b0bfa3305039e1e30595912609613916
vipulchandan <plenteousvipul90009@gmail.com>
2023-05-23 22:46:09 +0530

    Added authrization middleware and status codes and error handling

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const authenticate = function(req, res, next) {
+const auth = function(req, res, next) {
     try {
         //check the token in request header
         let token = req.headers["x-Auth-Token"];
@@ -9,54 +9,39 @@ const authenticate = function(req, res, next) {
         }
         
         if(!token) {
-            return res.status(401).json({ status: false, msg: "token must be present!" });
+            return res.status(401).send({ 
+                status: false, 
+                msg: "token must be present!" 
+            });
         }
         
         //validate this token
         let decodedToken = jwt.verify(token, "vipul-secret-key");
         if(!decodedToken){
-            return res.status(401).json({ status: false, msg: "Invalid token" });
-        }
-
-        next();
-
-    } catch(err) {
-        res.status(500).json({ status: false, msg: err.message });
-    }
-}
-
-
-const authorise = function(req, res, next) {
-    try {
-        //check the token in request header
-        let token = req.headers["x-Auth-Token"];
-        if(!token) {
-            token = req.headers["x-auth-token"];
-        }
-        
-        if(!token) {
-            return res.status(401).json({ status: false, msg: "token must be present!" });
-        }
-        
-        //validate this token
-        let decodedToken = jwt.verify(token, "vipul-secret-key");
-        if(!decodedToken){
-            return res.status(401).json({ status: false, msg: "Invalid token" });
+            return res.status(401).send({ 
+                status: false, 
+                msg: "Invalid token" 
+            });
         }
         
         // comapre the logged in user's id and the id in request
         let userToBeModified = req.params.userId;
-
         let userLoggedIn = decodedToken.userId;
 
         if(userLoggedIn != userToBeModified) {
-            return res.status(403).json({ status: false, msg: "User logged in is not allowed to modify requested user data!" });
+            return res.status(403).send({ 
+                status: false, 
+                msg: "User logged in is not allowed to modify requested user data!" 
+            });
         }
 
         next()
     } catch (err) {
-        res.status(500).json({ status: false, msg: err.message });
+        res.status(500).send({ 
+            status: false, 
+            msg: err.message 
+        });
     }
 }
 
-module.exports = { authenticate, authorise }
+module.exports.auth = auth
